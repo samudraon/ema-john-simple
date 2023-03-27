23c2b450005450080ac18d88a2a8b42f9f94e97a
samudraon <samudraon@gmailcom>
2023-03-27 12:08:49 +0600

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Shop from './Components/Header/Shop/Shop'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Shop />
     </div>
   )
 }
