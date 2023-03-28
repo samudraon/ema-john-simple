79ad0eeda2d8bba8dd2b3011e06411d665f7826c
samudraon <samudraon@gmailcom>
2023-03-28 23:25:58 +0600

    show data from cart

--- src/Components/Header/Shop/Shop.jsx
+++ src/Components/Header/Shop/Shop.jsx
@@ -33,9 +33,22 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // setCart(newCart);
+        // addToDb(product.id)
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
-        addToDb(product.id)
+        addToDb(product.id);
     }
 
     return (
